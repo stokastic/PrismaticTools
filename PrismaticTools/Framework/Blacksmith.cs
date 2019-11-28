0e1baf58251606737cb36cb402f4dec1b638c7b1
Seawolf <sean.klomparens@gmail.com>
2019-11-27 20:37:03 -0800

    Update for SMAPI 3.0
    Update for compatibility with SMAPI 3.0 and SDV 1.4

--- PrismaticTools/Framework/Blacksmith.cs
+++ PrismaticTools/Framework/Blacksmith.cs
@@ -4,23 +4,29 @@ using StardewValley;
 using StardewValley.Menus;
 using StardewValley.Tools;
 
-namespace PrismaticTools.Framework {
-    internal class BlacksmithInitializer {
+namespace PrismaticTools.Framework
+{
+    internal class BlacksmithInitializer
+    {
         private static readonly int UpgradeCost = ModEntry.Config.PrismaticToolCost;
 
-        public static void Init(IModEvents events) {
+        public static void Init(IModEvents events)
+        {
             events.Display.MenuChanged += OnMenuChanged;
         }
 
         /// <summary>Raised after a game menu is opened, closed, or replaced.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event arguments.</param>
-        private static void OnMenuChanged(object sender, MenuChangedEventArgs e) {
-            if (!(e.NewMenu is ShopMenu menu)) {
+        private static void OnMenuChanged(object sender, MenuChangedEventArgs e)
+        {
+            if (!(e.NewMenu is ShopMenu menu))
+            {
                 return;
             }
             List<int> categories = ModEntry.ModHelper.Reflection.GetField<List<int>>(menu, "categoriesToSellHere").GetValue();
-            if (!categories.Contains(Object.GemCategory) || !categories.Contains(Object.mineralsCategory) || !categories.Contains(Object.metalResources)) {
+            if (!categories.Contains(Object.GemCategory) || !categories.Contains(Object.mineralsCategory) || !categories.Contains(Object.metalResources))
+            {
                 return;
             }
             Farmer who = Game1.player;
@@ -31,25 +37,29 @@ namespace PrismaticTools.Framework {
             Tool toolFromName4 = who.getToolFromName("Hoe");
             Tool tool;
 
-            List<Item> forSale = ModEntry.ModHelper.Reflection.GetField<List<Item>>(menu, "forSale").GetValue();
-            Dictionary<Item, int[]> stock = ModEntry.ModHelper.Reflection.GetField<Dictionary<Item, int[]>>(menu, "itemPriceAndStock").GetValue();
+            List<ISalable> forSale = menu.forSale;
+            Dictionary<ISalable, int[]> stock = menu.itemPriceAndStock;
 
-            if (toolFromName1 != null && toolFromName1.UpgradeLevel == 4) {
+            if (toolFromName1 != null && toolFromName1.UpgradeLevel == 4)
+            {
                 tool = new Axe { UpgradeLevel = 5 };
                 forSale.Add(tool);
                 stock.Add(tool, new[] { UpgradeCost, 1, PrismaticBarItem.INDEX });
             }
-            if (toolFromName2 != null && toolFromName2.UpgradeLevel == 4) {
+            if (toolFromName2 != null && toolFromName2.UpgradeLevel == 4)
+            {
                 tool = new WateringCan { UpgradeLevel = 5 };
                 forSale.Add(tool);
                 stock.Add(tool, new[] { UpgradeCost, 1, PrismaticBarItem.INDEX });
             }
-            if (toolFromName3 != null && toolFromName3.UpgradeLevel == 4) {
+            if (toolFromName3 != null && toolFromName3.UpgradeLevel == 4)
+            {
                 tool = new Pickaxe { UpgradeLevel = 5 };
                 forSale.Add(tool);
                 stock.Add(tool, new[] { UpgradeCost, 1, PrismaticBarItem.INDEX });
             }
-            if (toolFromName4 != null && toolFromName4.UpgradeLevel == 4) {
+            if (toolFromName4 != null && toolFromName4.UpgradeLevel == 4)
+            {
                 tool = new Hoe { UpgradeLevel = 5 };
                 forSale.Add(tool);
                 stock.Add(tool, new[] { UpgradeCost, 1, PrismaticBarItem.INDEX });
