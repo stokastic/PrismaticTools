6c408d71fc9abd1da48164207e3af1726ca1aa59
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2022-05-07 17:04:05 -0400

    reduce repetition in asset editor

--- PrismaticTools/Framework/AssetEditor.cs
+++ PrismaticTools/Framework/AssetEditor.cs
@@ -13,27 +13,39 @@ namespace PrismaticTools.Framework {
         private string sprinklerDesc => ModEntry.ModHelper.Translation.Get("prismaticSprinkler.description");
 
         public void OnAssetRequested(AssetRequestedEventArgs e) {
+            // new item sprites
             if (e.NameWithoutLocale.IsEquivalentTo("Maps/springobjects")) {
                 e.Edit(asset => {
+                    var editor = asset.AsImage();
+
                     Texture2D bar = ModEntry.ModHelper.ModContent.Load<Texture2D>("assets/prismaticBar.png");
                     Texture2D sprinkler = ModEntry.ModHelper.ModContent.Load<Texture2D>("assets/prismaticSprinkler.png");
-                    Texture2D old = asset.AsImage().Data;
-                    asset.ReplaceWith(new Texture2D(Game1.graphics.GraphicsDevice, old.Width, System.Math.Max(old.Height, 1200 / 24 * 16)));
-                    asset.AsImage().PatchImage(bar, targetArea: this.GetRectangle(PrismaticBarItem.INDEX));
-                    asset.AsImage().PatchImage(sprinkler, targetArea: this.GetRectangle(PrismaticSprinklerItem.INDEX));
+
+                    asset.ReplaceWith(new Texture2D(Game1.graphics.GraphicsDevice, editor.Data.Width, System.Math.Max(editor.Data.Height, 1200 / 24 * 16)));
+                    editor.PatchImage(bar, targetArea: this.GetRectangle(PrismaticBarItem.INDEX));
+                    editor.PatchImage(sprinkler, targetArea: this.GetRectangle(PrismaticSprinklerItem.INDEX));
                 });
-            } else if (e.NameWithoutLocale.IsEquivalentTo("Data/ObjectInformation")) {
+            }
+
+            // new item data
+            else if (e.NameWithoutLocale.IsEquivalentTo("Data/ObjectInformation")) {
                 e.Edit(asset => {
-                    asset.AsDictionary<int, string>().Data.Add(PrismaticBarItem.INDEX, $"{this.barName}/{PrismaticBarItem.PRICE}/{PrismaticBarItem.EDIBILITY}/{PrismaticBarItem.TYPE} {PrismaticBarItem.CATEGORY}/{this.barName}/{this.barDesc}");
-                    asset.AsDictionary<int, string>().Data.Add(PrismaticSprinklerItem.INDEX, $"{this.sprinklerName}/{PrismaticSprinklerItem.PRICE}/{PrismaticSprinklerItem.EDIBILITY}/{PrismaticSprinklerItem.TYPE} {PrismaticSprinklerItem.CATEGORY}/{this.sprinklerName}/{this.sprinklerDesc}");
+                    var data = asset.AsDictionary<int, string>().Data;
+
+                    data.Add(PrismaticBarItem.INDEX, $"{this.barName}/{PrismaticBarItem.PRICE}/{PrismaticBarItem.EDIBILITY}/{PrismaticBarItem.TYPE} {PrismaticBarItem.CATEGORY}/{this.barName}/{this.barDesc}");
+                    data.Add(PrismaticSprinklerItem.INDEX, $"{this.sprinklerName}/{PrismaticSprinklerItem.PRICE}/{PrismaticSprinklerItem.EDIBILITY}/{PrismaticSprinklerItem.TYPE} {PrismaticSprinklerItem.CATEGORY}/{this.sprinklerName}/{this.sprinklerDesc}");
                 });
-            } else if (e.NameWithoutLocale.IsEquivalentTo("Data/CraftingRecipes")) {
+            }
+
+            // new recipes
+            else if (e.NameWithoutLocale.IsEquivalentTo("Data/CraftingRecipes")) {
                 e.Edit(asset => {
-                    IAssetDataForDictionary<string, string> oldDict = asset.AsDictionary<string, string>();
-                    Dictionary<string, string> newDict = new Dictionary<string, string>();
+                    var data = asset.AsDictionary<string, string>().Data;
+
                     // somehow the Dictionary maintains ordering, so reconstruct it with new sprinkler recipe immediately after prismatic
-                    foreach (string key in oldDict.Data.Keys) {
-                        newDict.Add(key, oldDict.Data[key]);
+                    Dictionary<string, string> newDict = new();
+                    foreach (string key in data.Keys) {
+                        newDict.Add(key, data[key]);
                         if (key.Equals("Iridium Sprinkler")) {
                             if (asset.Locale != "en")
                                 newDict.Add("Prismatic Sprinkler", $"{PrismaticBarItem.INDEX} 2 787 2/Home/{PrismaticSprinklerItem.INDEX}/false/Farming {PrismaticSprinklerItem.CRAFTING_LEVEL}/{this.sprinklerName}");
@@ -41,14 +53,20 @@ namespace PrismaticTools.Framework {
                                 newDict.Add("Prismatic Sprinkler", $"{PrismaticBarItem.INDEX} 2 787 2/Home/{PrismaticSprinklerItem.INDEX}/false/Farming {PrismaticSprinklerItem.CRAFTING_LEVEL}");
                         }
                     }
-                    asset.AsDictionary<string, string>().Data.Clear();
+
+                    data.Clear();
                     foreach (string key in newDict.Keys) {
-                        asset.AsDictionary<string, string>().Data.Add(key, newDict[key]);
+                        data.Add(key, newDict[key]);
                     }
                 });
-            } else if (e.NameWithoutLocale.IsEquivalentTo("TileSheets/tools")) {
+            }
+
+            // tool sprites
+            else if (e.NameWithoutLocale.IsEquivalentTo("TileSheets/tools")) {
                 e.Edit(asset => {
-                    asset.AsImage().PatchImage(ModEntry.ToolsTexture, patchMode: PatchMode.Overlay);
+                    var editor = asset.AsImage();
+
+                    editor.PatchImage(ModEntry.ToolsTexture, patchMode: PatchMode.Overlay);
                 });
             }
         }
